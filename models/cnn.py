6c2e87498b8ae9ebeb02cbfeeea453e4b705d8f0
Miles Turpin <milesaturpin@gmail.com>
2019-08-11 16:56:53 -0700

    lower factor cnn

--- models/cnn.py
+++ models/cnn.py
@@ -86,6 +86,9 @@ class LatentFactorCNN(BaseModel):
         super(LatentFactorCNN, self).__init__(
             optimizer, loss_fn, num_groups, args, experiment_dir, logger)
 
+        # TODO: move to superclass
+        self.encoder_size = args.encoder_size
+
     def _build_model(self):
         if self.model_size=='small':
             params=[16, 3, 32, 3, 256]
@@ -103,11 +106,27 @@ class LatentFactorCNN(BaseModel):
         self.layer1 = Dense(units=params[4], activation='relu')
         self.out = Dense(62, activation='softmax')
 
-
     def _build_latent_space(self):
         # (num_groups, z_dim), (num_groups, z_dim), (z_dim,)
         self.z_mu, self.z_sigma, self.z_prior = latent_normal_vector(
             shape=[self.num_groups[0], self.z_dim[0]])
+
+    # def _build_encoder(self):
+    #     if self.encoder_size=='small':
+    #         params=[4, 3, 8, 3, 32]
+    #     if self.encoder_size=='large':
+    #         params=[8, 3, 16, 3, 64]
+
+    #     self.reshape = Reshape((28,28,1), input_shape=(784,))
+    #     self.conv1 = Conv2D(filters=params[0], kernel_size=params[1],
+    #         padding='same', activation='relu')
+    #     self.pool1 = MaxPooling2D(2)
+    #     self.conv2 = Conv2D(filters=params[2], kernel_size=params[3],
+    #         padding='same', activation='relu')
+    #     self.pool2 = MaxPooling2D(2)
+    #     self.flatten = Flatten()
+    #     self.layer1 = Dense(units=params[4], activation='relu')
+    #     self.out = Dense(62, activation='softmax')
 
     def construct_variational_posterior(self, gid):
         # samples are shape (batch_size, z_dim)
@@ -131,6 +150,34 @@ class LatentFactorCNN(BaseModel):
         z = z_var_post.sample()
 
         x = tf.concat([x, z], axis=-1)
+        return self.out(x)
+
+
+class LowerLatentFactorCNN(LatentFactorCNN):
+    """
+    Latent variable CNN for FEMNIST data.
+    """
+
+    def __init__(self, optimizer, loss_fn, num_groups, args, experiment_dir, logger):
+        super(LowerLatentFactorCNN, self).__init__(
+            optimizer, loss_fn, num_groups, args, experiment_dir, logger)
+
+    def call(self, x, gid):
+        x = self.reshape(x)
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.pool2(x)
+        x = self.flatten(x)
+
+        z_var_post = self.construct_variational_posterior(gid)
+        kl_loss = tfd.kl_divergence(z_var_post, self.z_prior)
+        self.add_loss(lambda: kl_loss)
+        z = z_var_post.sample()
+
+        x = tf.concat([x, z], axis=-1)
+
+        x = self.layer1(x)
         return self.out(x)
 
 
