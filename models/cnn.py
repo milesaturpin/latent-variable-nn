9c3fde94dc48db5bf4a826c95e1ce6bb4ae338e6
Miles Turpin <milesaturpin@gmail.com>
2019-08-22 10:46:11 -0400

    test equivalence between concat and factored bias, fix add_loss lambda expression

--- models/cnn.py
+++ models/cnn.py
@@ -150,11 +150,80 @@ class LatentFactorCNN(BaseModel):
 
         z_var_post = self.construct_variational_posterior(gid)
         kl_loss = tfd.kl_divergence(z_var_post, self.z_prior)
-        self.add_loss(lambda: tf.reduce_sum(kl_loss))
+        self.add_loss(tf.reduce_sum(kl_loss))
         z = z_var_post.sample()
 
         x = tf.concat([x, z], axis=-1)
         return self.out(x)
+
+
+
+class LatentFactorCNN2(BaseModel):
+    """
+    Latent variable CNN for FEMNIST data.
+    """
+
+    def __init__(self, optimizer, loss_fn, train_size, num_groups, args, experiment_dir, logger):
+        super(LatentFactorCNN2, self).__init__(
+            optimizer, loss_fn, train_size, num_groups, args, experiment_dir, logger)
+
+        # TODO: move to superclass
+        #self.encoder_size = args.encoder_size
+
+    def _build_model(self):
+        if self.model_size=='small':
+            params=[16, 3, 32, 3, 256]
+        if self.model_size=='large':
+            params=[32, 5, 64, 5, 2048]
+
+        self.reshape = Reshape((28,28,1), input_shape=(784,))
+        self.conv1 = Conv2D(filters=params[0], kernel_size=params[1],
+            padding='same', activation='relu')
+        self.pool1 = MaxPooling2D(2)
+        self.conv2 = Conv2D(filters=params[2], kernel_size=params[3],
+            padding='same', activation='relu')
+        self.pool2 = MaxPooling2D(2)
+        self.flatten = Flatten()
+        self.layer1 = Dense(units=params[4], activation='relu')
+
+        self.bias_factors = Dense(62, use_bias=False)
+        # TODO: should I use bias here?
+        self.out = Dense(62)
+
+    def _build_latent_space(self):
+        # (num_groups, z_dim), (num_groups, z_dim), (z_dim,)
+        self.z_mu, self.z_sigma, self.z_prior = latent_normal_vector(
+            shape=[self.num_groups[0], self.z_dim[0]])
+
+    def construct_variational_posterior(self, gid):
+        # samples are shape (batch_size, z_dim)
+        post1 = latent_vector_variational_posterior(
+            self.z_mu, self.z_sigma, gid)
+        return post1
+
+
+    def call(self, x, gid):
+        x = self.reshape(x)
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.pool2(x)
+        x = self.flatten(x)
+        x = self.layer1(x)
+        x = self.out(x)
+
+        z_var_post = self.construct_variational_posterior(gid)
+        kl_loss = tfd.kl_divergence(z_var_post, self.z_prior)
+        self.add_loss(tf.reduce_sum(kl_loss))
+        z = z_var_post.sample()
+
+        bias = self.bias_factors(z)
+        #x = x + self.bias
+        x = x + bias
+        return tf.nn.softmax(x)
+
+
+
 
 
 class LowerLatentFactorCNN(LatentFactorCNN):
@@ -176,7 +245,7 @@ class LowerLatentFactorCNN(LatentFactorCNN):
 
         z_var_post = self.construct_variational_posterior(gid)
         kl_loss = tfd.kl_divergence(z_var_post, self.z_prior)
-        self.add_loss(lambda: tf.reduce_sum(kl_loss))
+        self.add_loss(tf.reduce_sum(kl_loss))
         z = z_var_post.sample()
 
         x = tf.concat([x, z], axis=-1)
@@ -232,7 +301,7 @@ class DoubleLatentCNN(BaseModel):
             self.construct_variational_posterior(gid))
         kl_loss = tfd.kl_divergence(z_var_post, self.z_prior)
         kl_loss2 = tfd.kl_divergence(z2_var_post, self.z2_prior)
-        self.add_loss(lambda: tf.reduce_sum(kl_loss) + tf.reduce_sum(kl_loss2))
+        self.add_loss(tf.reduce_sum(kl_loss) + tf.reduce_sum(kl_loss2))
 
         x = self.reshape(x)
         x = self.conv1(x)
@@ -310,7 +379,7 @@ class LatentBiasCNN(BaseModel):
         bias_posterior = self.construct_variational_posterior(gid)
         z_bias = bias_posterior.sample()
         kl_loss = tfd.kl_divergence(bias_posterior, self.z_prior)
-        self.add_loss(lambda: tf.reduce_sum(kl_loss))
+        self.add_loss(tf.reduce_sum(kl_loss))
 
         x = self.out(x)
         return tf.nn.softmax(x + z_bias)
@@ -352,10 +421,10 @@ class LatentWeightCNN(BaseModel):
 
 
         # self.weight_factors = tf.Variable(tfd.Normal(0.,0.1).sample(10,256*62))
-        self.weight_factors = Dense(units=params[4]*62, use_bias=False)
+        self.weight_factors = Dense(units=params[4]*62)
 
         #self.bias = tf.Variable(tfd.Normal(0.2,0.1).sample(62))
-        self.bias_factors = Dense(units=62, use_bias=False)
+        self.bias_factors = Dense(units=62)
 
         #self.out = tfpl.DenseFlipout(62, activation='softmax')
         #self.out = Dense(62, activation='softmax')
@@ -378,10 +447,10 @@ class LatentWeightCNN(BaseModel):
 
     def _build_latent_space(self):
         self.z_mu, self.z_sigma, self.z_prior = latent_normal_vector(
-            shape=[self.num_groups[0], 10])
+            shape=[self.num_groups[0], self.z_dim[0]])
 
         self.z2_mu, self.z2_sigma, self.z2_prior = latent_normal_vector(
-            shape=[self.num_groups[0], 16])
+            shape=[self.num_groups[0], self.z_dim[1]])
 
         # self.z3_mu, self.z3_sigma, self.z3_prior = latent_normal_vector(
         #     shape=[self.num_groups[0], 4])
@@ -402,16 +471,17 @@ class LatentWeightCNN(BaseModel):
         return post1, post2
 
     def call(self, x, gid):
-        #z_var_post = (
-        #   self.construct_variational_posterior(gid))
-        #kl_loss = tfd.kl_divergence(z_var_post, self.z_prior)
-        #self.add_loss(tf.reduce_sum(kl_loss))
+        z_var_post, z2_var_post = (
+          self.construct_variational_posterior(gid))
+        kl_loss = tfd.kl_divergence(z_var_post, self.z_prior)
+        kl_loss2 = tfd.kl_divergence(z2_var_post, self.z2_prior)
+        self.add_loss(tf.reduce_sum(kl_loss) + tf.reduce_sum(kl_loss2))
 
-        z = tf.gather(self.z_mu, gid)
-        self.add_loss(-1*tf.reduce_sum(self.z_prior.log_prob(z)))
+        # z = tf.gather(self.z_mu, gid)
+        # self.add_loss(-1*tf.reduce_sum(self.z_prior.log_prob(z)))
 
-        z2 = tf.gather(self.z2_mu, gid)
-        self.add_loss(-1*tf.reduce_sum(self.z2_prior.log_prob(z2)))
+        # z2 = tf.gather(self.z2_mu, gid)
+        # self.add_loss(-1*tf.reduce_sum(self.z2_prior.log_prob(z2)))
 
         # z3 = tf.gather(self.z3_mu, gid)
         # self.add_loss(-1*tf.reduce_sum(self.z3_prior.log_prob(z3)))
@@ -442,7 +512,8 @@ class LatentWeightCNN(BaseModel):
 
 
 
-        #z = z_var_post.sample()
+        z = z_var_post.sample()
+        z2 = z2_var_post.sample()
 
         #print(tf.reduce_mean(tf.math.abs(z)))
 
