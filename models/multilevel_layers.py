039a8ba903ea2fb72057325e4c97f46158ed8769
Miles Turpin <milesaturpin@gmail.com>
2019-10-11 01:07:50 -0400

    tfpdense, my multilevel dense, attempt tfp multilevel dense

--- models/multilevel_layers.py
+++ models/multilevel_layers.py
@@ -67,7 +67,163 @@ class MyDense(tf.keras.layers.Layer):
 
 
 
-class TFPMultilevelDense(tf.keras.layers.Layer):
+
+
+class MyMultilevelDense(tf.keras.layers.Layer):
+
+    def __init__(self, units, num_groups,
+                 multilevel_weights=True, multilevel_bias=True,
+                 #fixed_prior=True,
+                 kl_weight=None,
+                 kl_use_exact=False,
+                 activation=None,
+                 use_bias=True,
+                 **kwargs):
+        super(MyMultilevelDense, self).__init__(**kwargs)
+        #self._kl_divergence_fn = _make_kl_divergence_penalty(
+        #    kl_use_exact, weight=kl_weight)
+        self.units = int(units)
+        self.num_groups = num_groups
+        self.multilevel_weights = multilevel_weights
+        self.multilevel_bias = multilevel_bias
+        #self.fixed_prior = fixed_prior
+        self.use_bias = use_bias
+        self.activation = tf.keras.activations.get(activation)
+        self.input_spec = [
+            tf.keras.layers.InputSpec(min_ndim=2),
+            tf.keras.layers.InputSpec(ndim=1)]
+
+
+    def build(self, input_shape):
+
+        x_input_shape, gid_input_shape = input_shape
+        last_dim = x_input_shape[-1]
+        self.input_spec = [
+            tf.keras.layers.InputSpec(min_ndim=2, axes={-1: last_dim}),
+            tf.keras.layers.InputSpec(ndim=1)]
+
+        self.w_mu = self.add_weight(
+            shape=(self.num_groups, self.units, last_dim),
+            initializer='random_normal',
+            trainable=True,
+            name='kernel_mu')
+        self.w_sigma = self.add_weight(
+            shape=(self.num_groups, self.units, last_dim),
+            initializer='random_normal',
+            trainable=True,
+            name='kernel_sigma')
+        self.w0_mu = self.add_weight(
+            shape=(self.units, last_dim),
+            initializer='random_normal',
+            trainable=True,
+            name='kernel_prior_mu')
+        self.w0_sigma = self.add_weight(
+            shape=(self.units, last_dim),
+            initializer='random_normal',
+            #tf.constant_initializer(100.)
+            trainable=True,
+            name='kernel_prior_sigma')
+
+        self.b_mu = self.add_weight(
+            shape=(self.num_groups, self.units),
+            initializer='random_normal',
+            trainable=True,
+            name='bias_mu')
+        self.b_sigma = self.add_weight(
+            shape=(self.num_groups, self.units),
+            initializer='random_normal',
+            trainable=True,
+            name='bias_sigma')
+        self.b0_mu = self.add_weight(
+            shape=(self.units,),
+            initializer='random_normal',
+            trainable=True,
+            name='bias_prior_mu')
+        self.b0_sigma = self.add_weight(
+            shape=(self.units,),
+            initializer='random_normal',
+            trainable=True,
+            name='bias_prior_sigma')
+
+        super(MyMultilevelDense, self).build(input_shape)
+
+    def sample_posterior(self, mu, sigma, gid):
+        mu = tf.gather(mu, gid)
+        sigma = tf.gather(sigma, gid)
+        # By sampling after gather, I use different noise for each sample
+        eps = np.random.randn(*mu.shape)
+        #print(eps.shape, mu.shape, sigma.shape)
+        c = np.log(np.expm1(1.))
+        sigma_plus = tf.nn.softplus(c + sigma)
+        samp = mu + sigma_plus*eps
+        return samp
+
+
+    def compute_kl(self, mu1, sigma1, mu2, sigma2, gid):
+        #print(mu1, gid)
+        mu1 = tf.gather(mu1, gid)
+        sigma1 = tf.gather(sigma1, gid)
+        c = np.log(np.expm1(1.))
+        sigma1_plus = tf.nn.softplus(c + sigma1)
+        sigma2_plus = tf.nn.softplus(c + sigma2)
+        kl = (
+            tf.math.log(sigma2_plus/sigma1_plus)
+            + (sigma1_plus**2 + (mu1-mu2)**2)/(2*sigma2_plus**2)
+            - 0.5)
+
+        return kl
+
+
+
+    def call(self, inputs):
+        x, gid = inputs
+
+        batch_size, num_features = x.shape
+        assert len(x.shape) >= 2, "Data is incorrect shape!"
+        assert len(gid.shape) == 1, "gid should be flat vector!"
+
+        w_kl_loss = self.compute_kl(self.w_mu, self.w_sigma, self.w0_mu, self.w0_sigma, gid)
+        b_kl_loss = self.compute_kl(self.b_mu, self.b_sigma, self.b0_mu, self.b0_sigma, gid)
+        self.add_loss(tf.reduce_sum(w_kl_loss) + tf.reduce_sum(b_kl_loss))
+
+        w = self.sample_posterior(self.w_mu, self.w_sigma, gid)
+        b = self.sample_posterior(self.b_mu, self.b_sigma, gid)
+
+        # B: batch size, p: num_features, u: num_units
+        einsum_matrix_mult = '{},Bp->Bu'.format(
+            'Bup' if self.multilevel_weights else 'up')
+        #x = tf.einsum(einsum_matrix_mult, w, x)
+        #print(w.shape, x.shape)
+        outputs = tf.einsum(einsum_matrix_mult, w, x)
+
+        target_shape = (batch_size, self.units)
+        msg = "output is shape {}, when should be shape {}".format(outputs.shape, target_shape)
+        assert outputs.shape == target_shape, msg
+        assert len(outputs.shape) == 2, "Output is wrong shape!"
+
+        if self.use_bias:
+            #outputs = tf.nn.bias_add(outputs, b)
+            outputs = outputs + b
+
+        if self.activation is not None:
+            outputs = self.activation(outputs)
+
+        return outputs
+
+    def get_config(self):
+        config = super(MyMultilevelDense, self).get_config()
+        config.update({
+            'units': self.units,
+            'num_groups': self.num_groups,
+            'multilevel_weights' : self.multilevel_weights,
+            'multilevel_bias' : self.multilevel_bias,
+            'use_bias' : self.use_bias,
+            'activation' : self.activation,
+            'input_spec' : self.input_spec})
+        return config
+
+
+class TFPDense(tf.keras.layers.Layer):
 
     def __init__(self, units, num_groups,
                  multilevel_weights=False, multilevel_bias=True,
@@ -78,10 +234,8 @@ class TFPMultilevelDense(tf.keras.layers.Layer):
                  activation=None,
                  use_bias=True,
                  **kwargs):
-        super(TFPMultilevelDense, self).__init__(**kwargs)
+        super(TFPDense, self).__init__(**kwargs)
         self._make_posterior_fn = make_posterior_fn if make_posterior_fn is not None else self._default_posterior_trainable
-        #print('NOTE: change this')
-        #self._make_prior_fn = make_posterior_fn if make_posterior_fn is not None else self._default_posterior_trainable
         self._make_prior_fn = make_prior_fn if make_prior_fn is not None else self._default_prior_trainable
         self._kl_divergence_fn = _make_kl_divergence_penalty(
             kl_use_exact, weight=kl_weight)
@@ -98,14 +252,6 @@ class TFPMultilevelDense(tf.keras.layers.Layer):
 
 
     def build(self, input_shape):
-        # self.multi_w_mu = self.add_weight(
-        #     shape=(self.num_groups, x_input_shape[-1], self.units),
-        #     initializer='random_normal',
-        #     trainable=True)
-        # self.multi_b_mu = self.add_weight(
-        #     shape=(self.num_groups, self.units),
-        #     initializer='zeros',
-        #     trainable=True)
 
         x_input_shape, gid_input_shape = input_shape
         last_dim = x_input_shape[-1]
@@ -120,44 +266,7 @@ class TFPMultilevelDense(tf.keras.layers.Layer):
             last_dim * self.units,
             self.units if self.use_bias else 0)
 
-        # # Note the multiplication of the last 2 terms
-        # if self.multilevel_weights:
-        #     weight_rvs = init_mean_field_vectors(
-        #         shape=(self.num_groups, x_input_shape[-1] * self.units),
-        #         fixed_prior=self.fixed_prior)
-
-        #     if self.fixed_prior:
-        #         self.w_mu, self.w_sigma, self.w_prior = weight_rvs
-        #     else:
-        #         (self.w_mu, self.w_sigma, self.w_prior,
-        #          self.w0_mu, self.w0_sigma, self.w0_prior) = weight_rvs
-        # else:
-        #     #self.w = Dense(self.units)
-        #     self.w = self.add_weight(
-        #         shape=(x_input_shape[-1], self.units),
-        #         initializer='random_normal',
-        #         trainable=True)
-        #     #self.w = tf.Variable(tfd.Normal(0,0.1).sample([x_input_shape[-1], self.units]))
-
-        # if self.multilevel_bias:
-        #     bias_rvs = init_mean_field_vectors(
-        #         shape=(self.num_groups, self.units),
-        #         fixed_prior=self.fixed_prior,
-        #         mu_initializer='zeros')
-
-        #     if self.fixed_prior:
-        #         self.b_mu, self.b_sigma, self.b_prior = bias_rvs
-        #     else:
-        #         (self.b_mu, self.b_sigma, self.b_prior,
-        #          self.b0_mu, self.b0_sigma, self.b0_prior) = bias_rvs
-        # else:
-        #     self.b = self.add_weight(
-        #         shape=(self.units,),
-        #         initializer='zeros',
-        #         trainable=True)
-        #     #self.b = tf.Variable(np.zeros((self.units), dtype=np.float32))
-
-        super(TFPMultilevelDense, self).build(input_shape)
+        super(TFPDense, self).build(input_shape)
 
     def construct_posterior(self, z_mu, z_sigma, z_prior, gid):
         """Build the posterior and compute the loss."""
@@ -172,7 +281,6 @@ class TFPMultilevelDense(tf.keras.layers.Layer):
 
     def _default_posterior_trainable(self, kernel_size, bias_size=0, dtype=None):
         # Specify the surrogate posterior over `keras.layers.Dense` `kernel` and `bias`.
-        print(kernel_size, bias_size)
         n = kernel_size + bias_size
         c = np.log(np.expm1(1.))
         return tf.keras.Sequential([
@@ -196,6 +304,7 @@ class TFPMultilevelDense(tf.keras.layers.Layer):
 
 
 
+
     def call(self, inputs):
         x, gid = inputs
 
@@ -203,43 +312,160 @@ class TFPMultilevelDense(tf.keras.layers.Layer):
         assert len(x.shape) >= 2, "Data is incorrect shape!"
         assert len(gid.shape) == 1, "gid should be flat vector!"
 
-        # if self.multilevel_weights:
-        #     w_post = self.construct_posterior(
-        #         self.w_mu, self.w_sigma, self.w_prior, gid)
-
-        #     # Sample is of shape (B, num_features*units), but
-        #     # we want it as shape (B, num_features, units). In
-        #     # keras you don't explicitly state batch sizes - it's
-        #     # handled at runtime.
-        #     w = Reshape((-1, self.units))(w_post.sample())
-        # else:
-        #     # w is of shape (num_features, units)
-        #     w = self.w
-
-        # if self.multilevel_bias:
-        #     b_post = self.construct_posterior(
-        #         self.b_mu, self.b_sigma, self.b_prior, gid)
-
-        #     # Sample is of shape (B, units). In keras you
-        #     # don't explicitly state batch sizes - it's
-        #     # handled at runtime.
-        #     b = b_post.sample()
-        # else:
-        #     # w is of shape (units,)
-        #     b = self.b
-
-        #print('before expand', x)
-        #x = tf.expand_dims(x, axis=-1)
-        #print('after expand', x)
-        #print(x.shape, w.shape)
-        #x = tf.matmul(w, x, transpose_a=True)
-        #print('after mult', x)
-        #x = tf.squeeze(x, axis=-1)
-
         q = self._posterior(x)
         r = self._prior(x)
         #print(q,r)
+        #self.add_loss(self._kl_divergence_fn(q, r))
+        kl_loss = tfd.kl_divergence(q, r)
+        self.add_loss(tf.reduce_sum(kl_loss))
+
+        w = tf.convert_to_tensor(value=q)
+        prev_units = self.input_spec[0].axes[-1]
+        if self.use_bias:
+          split_sizes = [prev_units * self.units, self.units]
+          kernel, bias = tf.split(w, split_sizes, axis=-1)
+        else:
+          kernel, bias = w, None
+
+        kernel = tf.reshape(kernel, shape=tf.concat([
+            tf.shape(input=kernel)[:-1],
+            [prev_units, self.units],
+        ], axis=0))
+
+        #outputs = tf.matmul(inputs, kernel)
+
+        # B: batch size, p: num_features, u: num_units
+        einsum_matrix_mult = '{},Bp->Bu'.format(
+            'Bup' if self.multilevel_weights else 'up')
+        #x = tf.einsum(einsum_matrix_mult, w, x)
+        outputs = tf.einsum(einsum_matrix_mult, kernel, x)
+
+        target_shape = (batch_size, self.units)
+        msg = "x is shape {}, when should be shape {}".format(x.shape, target_shape)
+        assert x.shape == target_shape, msg
+        assert len(outputs.shape) == 2, "Output is wrong shape!"
+
+        if self.use_bias:
+            outputs = tf.nn.bias_add(outputs, bias)
+
+        if self.activation is not None:
+            outputs = self.activation(outputs)
+
+        return outputs
+
+
+
+
+class TFPMultilevelDense(tf.keras.layers.Layer):
+
+    def __init__(self, units, num_groups,
+                 multilevel_weights=False, multilevel_bias=True,
+                 #fixed_prior=True,
+                 make_posterior_fn=None, make_prior_fn=None,
+                 kl_weight=None,
+                 kl_use_exact=False,
+                 activation=None,
+                 use_bias=True,
+                 **kwargs):
+        super(TFPMultilevelDense, self).__init__(**kwargs)
+        self._make_posterior_fn = make_posterior_fn if make_posterior_fn is not None else self._default_posterior_trainable
+        self._make_prior_fn = make_prior_fn if make_prior_fn is not None else self._default_prior_trainable
+        self._kl_divergence_fn = _make_kl_divergence_penalty(
+            kl_use_exact, weight=kl_weight)
+        self.units = int(units)
+        self.num_groups = num_groups
+        self.multilevel_weights = multilevel_weights
+        self.multilevel_bias = multilevel_bias
+        #self.fixed_prior = fixed_prior
+        self.use_bias = use_bias
+        self.activation = tf.keras.activations.get(activation)
+        self.input_spec = [
+            tf.keras.layers.InputSpec(min_ndim=2),
+            tf.keras.layers.InputSpec(ndim=1)]
+
+
+    def build(self, input_shape):
+
+        x_input_shape, gid_input_shape = input_shape
+        last_dim = x_input_shape[-1]
+        self.input_spec = [
+            tf.keras.layers.InputSpec(min_ndim=2, axes={-1: last_dim}),
+            tf.keras.layers.InputSpec(ndim=1)]
+
+        self._posterior = self._make_posterior_fn(
+            num_groups=self.num_groups,
+            kernel_size=last_dim * self.units,
+            bias_size=self.units if self.use_bias else 0)
+        self._prior = self._make_prior_fn(
+            last_dim * self.units,
+            self.units if self.use_bias else 0)
+
+        super(TFPMultilevelDense, self).build(input_shape)
+
+    def construct_posterior(self, z_mu, z_sigma, z_prior, gid):
+        """Build the posterior and compute the loss."""
+
+        z_post = build_normal_variational_posterior(
+            z_mu, z_sigma, gid)
+
+        kl_loss = tfd.kl_divergence(z_post, z_prior)
+        self.add_loss(tf.reduce_sum(kl_loss))
+        return z_post
+
+
+    def _default_posterior_trainable(self, num_groups, kernel_size, bias_size, dtype=None):
+        # Specify the surrogate posterior over `keras.layers.Dense` `kernel` and `bias`.
+        n = kernel_size + bias_size
+        c = np.log(np.expm1(1.))
+        return tf.keras.Sequential([
+            tfp.layers.VariableLayer([num_groups, 2 * n], dtype=dtype),
+            tfp.layers.DistributionLambda(lambda t: tfd.Independent(
+                tfd.Normal(loc=t[..., :n],
+                           scale=1e-5 + tf.nn.softplus(c + t[..., n:])),
+                reinterpreted_batch_ndims=1
+                )),
+        ])
+        # gid = tf.keras.layers.Input(shape=(1,))
+        # params = tfp.layers.VariableLayer([num_groups, 2 * n], dtype=dtype)
+        # batch_params = tf.gather(params, gid)
+        # batch_dist = tfp.layers.DistributionLambda(lambda t: tfd.Independent(
+        #     tfd.Normal(loc=t[..., :n],
+        #                 scale=1e-5 + tf.nn.softplus(c + t[..., n:])),
+        #     reinterpreted_batch_ndims=1))(batch_params)
+        # posterior = tf.keras.models.Model(inputs=gid, outputs=batch_dist)
+        # return posterior
+
+
+    def _default_prior_trainable(self, kernel_size, bias_size, dtype=None):
+        # Specify the prior over `keras.layers.Dense` `kernel` and `bias`.
+        n = kernel_size + bias_size
+        c = np.log(np.expm1(1.))
+        return tf.keras.Sequential([
+            tfp.layers.VariableLayer(2 * n, dtype=dtype),
+            tfp.layers.DistributionLambda(lambda t: tfd.Independent(
+                tfd.Normal(loc=t[..., :n],
+                           scale=1e-5 + tf.nn.softplus(c + t[..., n:])),
+                reinterpreted_batch_ndims=1)),
+        ])
+
+
+    def call(self, inputs):
+        x, gid = inputs
+
+        batch_size, num_features = x.shape
+        assert len(x.shape) >= 2, "Data is incorrect shape!"
+        assert len(gid.shape) == 1, "gid should be flat vector!"
+
+        q = self._posterior(x)
+
+        r = self._prior(x)
+
+        print( q,r)
+        #print(self._kl_divergence_fn(q, r))
         self.add_loss(self._kl_divergence_fn(q, r))
+        print('before', q)
+        q = tf.gather(q, gid)
+        print('after', q)
         #kl_loss = tfd.kl_divergence(q, r)
         #self.add_loss(tf.reduce_sum(kl_loss))
 
@@ -258,42 +484,25 @@ class TFPMultilevelDense(tf.keras.layers.Layer):
 
         #outputs = tf.matmul(inputs, kernel)
 
-
         # B: batch size, p: num_features, u: num_units
         einsum_matrix_mult = '{},Bp->Bu'.format(
             'Bup' if self.multilevel_weights else 'up')
         #x = tf.einsum(einsum_matrix_mult, w, x)
         outputs = tf.einsum(einsum_matrix_mult, kernel, x)
 
-
-        #print('after squeeze', x)
-        #print('bias', b)
         target_shape = (batch_size, self.units)
         msg = "x is shape {}, when should be shape {}".format(x.shape, target_shape)
         assert x.shape == target_shape, msg
-
-        #out = self.activation(x + b)
-        #print('ou', out.shape)
         assert len(outputs.shape) == 2, "Output is wrong shape!"
-        #print(out)
-
-        #print(self.get_weights())
 
         if self.use_bias:
-            outputs = tf.nn.bias_add(outputs, bias)
+            # outputs = tf.nn.bias_add(outputs, bias)
+            outputs = outputs + bias
 
         if self.activation is not None:
             outputs = self.activation(outputs)
 
         return outputs
-
-    def get_config(self):
-        config = super(TFPMultilevelDense, self).get_config()
-        # TODO: actually fill in
-        config.update({'units': self.units, 'num_groups': self.num_groups,
-            'multilevel_weights' : False, 'multilevel_bias' : True,
-            'fixed_prior' : True, 'activation' : None})
-        return config
 
 
 
